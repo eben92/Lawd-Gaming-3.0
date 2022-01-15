851112e59f2e480e3a0500b051de3794b7f06859
EBEN <pappy.rhutmannjnrolege@gmail.com>
2022-01-15 17:48:19 +0000

    added reg auth [ validator && api ]

---
+++ utils/valid.js
@@ -0,0 +1,19 @@
+const validator = (name, email, password, cf_password) => {
+  if (!name || !email || !password) {
+    return "Please add all fields";
+  }
+
+  if (!validateEmail(email)) return "Invalid email.";
+
+  if (!password.length < 6) return "Password must be at least 6 characters.";
+
+  if (password !== cf_password) return "Confirm password didn't match.";
+};
+
+function validateEmail(email) {
+  var re =
+    /^(([^<>()[]\\.,;:\s@\"]+(\.[^<>()[]\\.,;:\s@\"]+)*)|(\".+\"))@(([[0-9]{1,3}\‌​.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+export default validator;
