e0ffd09ab9985a2a9bc641f17a4de7b099538750
EBEN <pappy.rhutmannjnrolege@gmail.com>
2022-01-15 18:25:44 +0000

    added redux store

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import "../styles/globals.css";
 import "semantic-ui-css/semantic.min.css";
-
+import { DataProvider } from "../store/GlobalState";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <DataProvider>
+      <Component {...pageProps} />
+    </DataProvider>
+  );
 }
 
 export default MyApp;
